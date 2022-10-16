d6d04fc75b7d92bb07d8e328136fb11e6265f3d3
Samir70 <sbetmouni@talktalk.net>
2022-10-16 11:21:06 +0100

    add an accordion from antDesign

--- src/components/showCourseProgress.vue
+++ src/components/showCourseProgress.vue
@@ -1,13 +1,21 @@
 <script setup>
+import { ref } from "vue";
 const props = defineProps({
     course: Object
 })
+const activeKey = ref([]);
 </script>
 <template>
     <div id="course-display-box">
         <p>Course name: {{course.courseName}}</p>
         <p>Modules: {{course.modules}}</p>
+        <a-collapse v-model:activeKey="activeKey" accordion>
+            <a-collapse-panel v-for="module of course.modules" v-bind:key="module" v-bind:header="module">
+                <p>content of module {{course.modules.module}}</p>
+            </a-collapse-panel>
+        </a-collapse>
     </div>
 </template>
 <style>
+
 </style>
