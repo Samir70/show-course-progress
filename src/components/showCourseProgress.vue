b40158fe2e0bda09c076ed37d8ed75c4b769bda7
Samir70 <sbetmouni@talktalk.net>
2022-10-16 11:47:32 +0100

    show chapters as an accordion

--- src/components/showCourseProgress.vue
+++ src/components/showCourseProgress.vue
@@ -10,8 +10,14 @@ const activeKey = ref([]);
         <p>Course name: {{course.courseName}}</p>
         <p>Modules: {{course.modules}}</p>
         <a-collapse v-model:activeKey="activeKey" accordion>
-            <a-collapse-panel v-for="module of course.modules" v-bind:key="module.key" v-bind:header="module.moduleName">
+            <a-collapse-panel v-for="module of course.modules" v-bind:key="module.key"
+                v-bind:header="module.moduleName">
                 <p>Chapters of module {{module.chapters}}</p>
+                <a-collapse accordion>
+                    <a-collapse-panel v-for="chapter of module.chapters" v-bind:key="chapter" v-bind:header="chapter">
+                        <p>Contents of chapter</p>
+                    </a-collapse-panel>
+                </a-collapse>
             </a-collapse-panel>
         </a-collapse>
     </div>
